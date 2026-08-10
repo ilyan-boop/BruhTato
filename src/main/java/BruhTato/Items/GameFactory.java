91e65fb942c310ae0e603003e603542768d113ee
Zachary Molinyawe <zachary_molinyawe@dlsu.edu.ph>
2026-08-10 16:44:18 +0800

    18: Updated the GameFactory where the enemy hitbox was changed to a circle.

--- src/main/java/BruhTato/Items/GameFactory.java
+++ src/main/java/BruhTato/Items/GameFactory.java
@@ -2,21 +2,30 @@ package BruhTato.Items;
 
 import BruhTato.Enemies.EnemyComponent;
 import BruhTato.Utils.EntityType;
-import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.EntityFactory;
 import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.entity.Spawns;
+import com.almasb.fxgl.entity.components.CollidableComponent;
+import com.almasb.fxgl.physics.BoundingShape;
+import com.almasb.fxgl.physics.HitBox;
+import javafx.geometry.Point2D;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
 
 public class GameFactory implements EntityFactory {
 
     @Spawns("enemy")
     public Entity newEnemy(SpawnData data) {
-        return FXGL.entityBuilder(data)
+        double radius = 50.0; // Radius matching enemy body size
+
+        return entityBuilder(data)
                 .type(EntityType.ENEMY)
-                .viewWithBBox(FXGL.texture("Melee_Enemy_FullHealth.png", 200, 200))
-                .with(new EnemyComponent(150.0))
-                .collidable()
+                // Single circular hitbox centered on the entity (removes square box)
+                .bbox(new HitBox(new Point2D(25, 25), BoundingShape.circle(radius)))
+                .view(texture("Melee_Enemy_FullHealth.png", 150, 150))
+                .with(new CollidableComponent(true))
+                .with(new EnemyComponent(3.5)) // Fixed speed matching player steps
                 .build();
     }
 }
